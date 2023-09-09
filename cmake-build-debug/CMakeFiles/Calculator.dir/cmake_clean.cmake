e830a5d13949eba522bf4bb40e2f5d0151a63e4f
Lightmon <56477197+lightmon233@users.noreply.github.com>
2023-09-09 23:28:19 +0800

    infix expressions intergrated into classes

---
+++ cmake-build-debug/CMakeFiles/Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calculator.dir/BaseExpression.cpp.obj"
+  "CMakeFiles/Calculator.dir/BaseExpression.cpp.obj.d"
+  "CMakeFiles/Calculator.dir/Digit.cpp.obj"
+  "CMakeFiles/Calculator.dir/Digit.cpp.obj.d"
+  "CMakeFiles/Calculator.dir/InfixExpression.cpp.obj"
+  "CMakeFiles/Calculator.dir/InfixExpression.cpp.obj.d"
+  "CMakeFiles/Calculator.dir/main.cpp.obj"
+  "CMakeFiles/Calculator.dir/main.cpp.obj.d"
+  "Calculator.exe"
+  "Calculator.exe.manifest"
+  "Calculator.pdb"
+  "libCalculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
